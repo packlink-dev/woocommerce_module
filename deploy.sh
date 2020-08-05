3e2dca68c584f6c0b1b73eea89f61b952372afe6
Petar Rankovic <petar.rankovic@logeecom.com>
2020-08-05 16:49:40 +0200

    Add url migration
    ISSUE PLCR18-8

--- deploy.sh
+++ deploy.sh
@@ -46,16 +46,14 @@ echo -e "\e[32mSTEP 4:\e[39m Copying resources from core to the integration..."
 root="$PWD";
 source="$PWD/packlink-pro-shipping/vendor/packlink/integration-core/src/BusinessLogic/Resources";
 destination="$PWD/packlink-pro-shipping/resources";
-if [ ! -d "$destination/images/carriers" ]; then
-  mkdir "$destination/images/carriers"
-fi
+
 if [ ! -d "$destination/js/core" ]; then
   mkdir "$destination/js/core"
 fi
 if [ ! -d "$destination/js/location-picker" ]; then
   mkdir "$destination/js/location-picker"
 fi
-cp -r ${source}/img/carriers/* ${destination}/images/carriers
+
 cp -r ${source}/js/* ${destination}/js/core
 cp -r ${source}/LocationPicker/js/* ${destination}/js/location-picker
 cp -r ${source}/LocationPicker/css/* ${destination}/css
