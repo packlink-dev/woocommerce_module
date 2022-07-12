2244e710e91ddc13c745aa4377e7907683d5e6eb
Branko Janjic <branko.janjic@logeecom.com>
2022-07-12 14:53:27 +0200

    CS-3363

--- src/resources/js/packlink-checkout.js
+++ src/resources/js/packlink-checkout.js
@@ -26,9 +26,9 @@ var Packlink = window.Packlink || {};
 
 	function initialize() {
 		modal        = document.getElementById( 'pl-picker-modal' );
+		let isModalLocationChanged = false;
 		closeButton  = document.getElementById( 'pl-picker-modal-close' );
 		updateButton = document.querySelector( "[name='calc_shipping']" );
-
 		[].forEach.call(
 			document.getElementsByName( 'packlink_show_image' ),
 			function (item) {
@@ -46,6 +46,10 @@ var Packlink = window.Packlink || {};
 					button.addEventListener(
 						'click',
 						function () {
+							if (!isModalLocationChanged) {
+								document.body.appendChild(modal);
+								isModalLocationChanged = true;
+							}
 							initLocationPicker();
 							modal.style.display = 'block';
 						}
