6b5ef71c6e1232b8a5c0fc781131acead23233f0
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2025-03-12 15:13:13 +0100

    Mark compatibility with WooCommerce 9.6.0 and WordPress 6.7.1
    ISSUE: CS-6515

--- src/packlink-pro-shipping.php
+++ src/packlink-pro-shipping.php
@@ -16,7 +16,7 @@
  * Text Domain: packlink-pro-shipping
  * Domain Path: /languages
  * WC requires at least: 3.0.0
- * WC tested up to: 9.4.2
+ * WC tested up to: 9.6.0
  */
 
 use Packlink\WooCommerce\Plugin;
