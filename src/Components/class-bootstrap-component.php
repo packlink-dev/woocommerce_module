facf83770358e153f8127d117413b2ede856a934
Milos Damjanovic <milos.damjanovic@logeecom.com>
2026-02-04 15:39:53 +0100

    Fixed weight unit conversion
    PL4TIASC-207

--- src/Components/class-bootstrap-component.php
+++ src/Components/class-bootstrap-component.php
@@ -28,12 +28,14 @@ use Packlink\BusinessLogic\CashOnDelivery\Services\OfflinePaymentsServices;
 use Packlink\BusinessLogic\Country\WarehouseCountryService;
 use Packlink\BusinessLogic\FileResolver\FileResolverService;
 use Packlink\BusinessLogic\Order\Interfaces\ShopOrderService;
+use Packlink\BusinessLogic\Order\OrderService;
 use Packlink\BusinessLogic\OrderShipmentDetails\Models\OrderShipmentDetails;
 use Packlink\BusinessLogic\Registration\RegistrationInfoService;
 use Packlink\BusinessLogic\Scheduler\Models\Schedule;
 use Packlink\BusinessLogic\ShipmentDraft\Models\OrderSendDraftTaskMap;
 use Packlink\BusinessLogic\ShipmentDraft\ShipmentDraftService;
 use Packlink\WooCommerce\Components\Services\Offline_Payments_Service;
+use Packlink\WooCommerce\Components\Services\Order_Service;
 use Packlink\WooCommerce\Components\Services\Shipment_Draft_Service;
 use Packlink\BusinessLogic\ShippingMethod\Interfaces\ShopShippingMethodService;
 use Packlink\BusinessLogic\ShippingMethod\Models\ShippingMethod;
@@ -61,6 +63,13 @@ class Bootstrap_Component extends BootstrapComponent {
 	 */
 	protected static function initServices() {
 		parent::initServices();
+
+		ServiceRegister::registerService(
+			OrderService::CLASS_NAME,
+			function () {
+				return Order_Service::getInstance();
+			}
+		);
 
 		ServiceRegister::registerService(
 			Serializer::CLASS_NAME,
