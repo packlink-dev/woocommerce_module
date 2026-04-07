3d002d5bb1ac5258a407cd6f9ee26f5b61548663
Milos Damjanovic <milos.damjanovic@logeecom.com>
2026-04-07 10:38:01 +0200

    Webhook responses updated

--- src/Controllers/class-packlink-integration-registration-webhook-controller.php
+++ src/Controllers/class-packlink-integration-registration-webhook-controller.php
@@ -11,6 +11,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+use Logeecom\Infrastructure\Logger\Logger;
+use Packlink\BusinessLogic\WebHook\Exceptions\WebhookAuthorizationException;
+use Packlink\BusinessLogic\WebHook\Exceptions\WebhookPayloadValidationException;
 use Packlink\BusinessLogic\WebHook\IntegrationRegistrationWebhookEventHandler;
 
 /**
@@ -39,8 +42,24 @@ class Packlink_Integration_Registration_Webhook_Controller extends Packlink_Base
 			$this->redirect404();
 		}
 
-		IntegrationRegistrationWebhookEventHandler::getInstance()->handle( $this->get_raw_input() );
+		try {
+			IntegrationRegistrationWebhookEventHandler::getInstance()->handle( $this->get_raw_input() );
 
-		$this->return_json( array( 'success' => true ), 200);
+			$this->return_json( array( 'success' => true ), 200 );
+
+		} catch ( WebhookAuthorizationException $e ) {
+			$this->return_json( array( 'success' => false, 'error' => $e->getMessage() ), 401 );
+
+		} catch ( WebhookPayloadValidationException $e ) {
+			$this->return_json( array( 'success' => false, 'error' => $e->getMessage() ), 400 );
+
+		} catch ( \Exception $e ) {
+			Logger::logError(
+				'Packlink registration webhook: unexpected error.',
+				'Core',
+				array( 'message' => $e->getMessage() )
+			);
+			$this->return_json( array( 'success' => false, 'error' => 'Internal server error.' ), 500 );
+		}
 	}
 }
